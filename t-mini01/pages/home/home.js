4c868be324af49ab670fa95e82d980a251a1a87f
心缘忆码 <little-jelly@users.noreply.github.com>
2020-03-17 11:21:36 +0800

    使用Lin-UI进行组件化编程实现六宫格

--- t-mini01/pages/home/home.js
+++ t-mini01/pages/home/home.js
@@ -1,12 +1,14 @@
 // pages/home/home.js
 import {Theme} from "../../model/theme";
 import {Banner} from "../../model/banner";
+import {Category} from "../../model/category";
 
 Page({
 
   data: {
     themeA:null,
-    bannerB:null
+    bannerB:null,
+      grid:[]
   },
 
   async onLoad(options) {
@@ -17,9 +19,11 @@ Page({
    async initAllData() {
        const themeA = await Theme.getHomeLocationA()
        const bannerB = await Banner.getHomeLocationB()
+       const grid = await Category.getGridCategory()
        this.setData({
            themeA:themeA[0],
-           bannerB:bannerB
+           bannerB:bannerB,
+           grid:grid
        })
    },
 
