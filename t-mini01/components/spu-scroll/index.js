2b82573a2b5fd7a53cf4f22b97862c2974e6d1df
心缘忆码 <little-jelly@users.noreply.github.com>
2020-03-25 21:33:35 +0800

    spu样式调整

---
+++ t-mini01/components/spu-scroll/index.js
@@ -0,0 +1,24 @@
+// components/spu-scroll/index.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    theme:Object,
+    spuList:Array
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
