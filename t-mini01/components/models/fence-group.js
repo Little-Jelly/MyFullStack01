37d4ddcc68f92d73d85a945f6030b1d631853a05
ZhangYoufa <207986989@qq.com>
2020-09-10 20:42:32 +0800

    代码重构，使用纯正的面向对象来整理数据！

--- t-mini01/components/models/fence-group.js
+++ t-mini01/components/models/fence-group.js
@@ -13,16 +13,12 @@ class FenceGroup{
             initFences(){
                 const matrix = this._createMatrix(this.skuList)
                 const fences = []
-                let currentJ = -1
-                matrix.forEach((element, i, j)=>{
-                    if(currentJ != j){
-                        // 开启一个新列，需要创建一个新的Fence
-                        currentJ = j
-                        fences[currentJ] = this._createFence(element)
-                    }
-                    fences[currentJ].pushValueTtile(element.value)
+                const AT = matrix.transpose()
+                AT.forEach(r=>{
+                    const fence = new Fence(r)
+                    fence.init()
+                    fences.push(fence)
                 })
-                console.log(fences)
             }
 
             _createFence(element){
--- t-mini01/components/models/fence.js
+++ t-mini01/components/models/fence.js
@@ -2,6 +2,16 @@
 
         class Fence{
             valueTitles = []
+            specs
+            constructor(specs) {
+                this.specs = specs
+            }
+
+            init(){
+                this.specs.forEach(s=>{
+                    this.pushValueTtile(s.value)
+                })
+            }
 
             pushValueTtile(title){
                 this.valueTitles.push(title)
