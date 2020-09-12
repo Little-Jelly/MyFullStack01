7bde2efc986a4401acad288abbfabd03976b9daf
ZhangYoufa <207986989@qq.com>
2020-09-12 22:54:35 +0800

    寻找潜在路径

--- t-mini01/components/models/judger.js
+++ t-mini01/components/models/judger.js
@@ -1,13 +1,23 @@
 import {SkuCode} from "./sku-code";
 import {CellStatus} from "../../core/enum";
+import {SkuPending} from "./sku-pending";
+import {Joiner} from "../../utils/joiner";
 
 
 class Judger{
         fenceGroup
         pathDict = []
+        skuPending
 
         constructor(fenceGroup) {
             this.fenceGroup = fenceGroup
+            this._initSkuPending()
+            this._initPathDict()
+
+        }
+
+        _initSkuPending(){
+            this.skuPending = new SkuPending()
         }
 
         _initPathDict(){
@@ -19,16 +29,43 @@ class Judger{
         }
 
         judge(cell, x, y){
-            console.log(cell, x, y)
-            this._changeCellStatus(cell, x, y)
+            this._changeCurrentCellStatus(cell, x, y)
+            this.fenceGroup.eachCell((cell, x, y)=>{
+                const path = this._findPotentialPath(cell, x, y)
+                console.log(path)
+            })
         }
 
-        _changeCellStatus(cell, x, y){
+        _findPotentialPath(cell, x, y){
+            const joiner = new Joiner('#')
+            for(let i=0; i< this.fenceGroup.fences.length;i++){
+                const selected = this.skuPending.findSelectedCellByX(i)
+                if(x === i){
+                    const cellCode = this._getCellCode(cell.spec)
+                    joiner.join(cellCode)
+                }
+                else {
+                    if(selected) {
+                        const selectedCellCode = this._getCellCode(selected.spec)
+                        joiner.join(selectedCellCode)
+                    }
+                }
+            }
+            return joiner.getStr()
+        }
+
+        _getCellCode(spec){
+            return spec.key_id + '-' + spec.value_id
+        }
+
+        _changeCurrentCellStatus(cell, x, y){
             if(cell.status === CellStatus.WAITING){
                 this.fenceGroup.fences[x].cells[y].status = CellStatus.SELECTED
+                this.skuPending.insertCell(cell, x)
             }
             if(cell.status == CellStatus.SELECTED){
                 this.fenceGroup.fences[x].cells[y].status = CellStatus.WAITING
+                this.skuPending.removeCell(x)
             }
         }
     }
---
+++ t-mini01/components/models/sku-pending.js
@@ -0,0 +1,20 @@
+
+    class SkuPending{
+        pending = []
+
+        constructor() {
+        }
+        insertCell(cell, x){
+            this.pending[x] = cell
+        }
+        removeCell(x){
+            this.pending[x] = null
+        }
+
+        findSelectedCellByX(x){
+            return this.pending[x]
+        }
+    }
+    export {
+        SkuPending
+    }
